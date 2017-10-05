c50510183e98e4de8d98815cee30c051306da1d3
Deysi <sude.90@gmail.com>
2017-10-05 16:27:32 -0500

    boton start, stop

--- ScoreBoard-master/player-scoreboard/app.jsx
+++ ScoreBoard-master/player-scoreboard/app.jsx
@@ -4,12 +4,12 @@ let PLAYERS = [
     score: 31,
     id: 1,
   },
-   {
+  {
     name: "Andree Hoskins",
     score: 35,
     id: 2,
   },
-   {
+  {
     name: "Alena Hoskins",
     score: 42,
     id: 3,
@@ -20,104 +20,169 @@ let PLAYERS = [
     id: 4,
   },
 ];
-class Modal{
-  constructor () {
-    
-      this.inputValue = null;
-      this.render = undefined;
-      this.player = PLAYERS;
-      this.callback = null;
-   }
-  
+
+'using strict';
+//import React from "react";
+//import ReactDOM from "react-dom"; 
+
+class TemporalComponent extends React.Component {
+
+  render() {
+    return (
+      <div>
+        {(new Date()).toLocaleTimeString()}
+      </div>
+    );
+  }
+
+}
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    }
+  }
+  render() {
+    const { title } = this.props;
+    const start = (e) => {
+      this.startTimer();
+    }
+    const stop = (e) => {
+      this.stopTimer();
+    }
+    return (
+      <div>
+        <h2> {title} </h2>
+
+        
+        <button onClick={start}> start </button>
+<button onClick={stop}> stop </button>
+
+        <p> {this.state.date.toLocaleTimeString()}</p>
+
+        <span> <TemporalComponent /> </span>
+      </div>
+    );
+  }
+  // componentDidMount
+  startTimer() {
+    this.timer = setInterval(() => {
+      this.setState({
+        date: new Date()
+      });
+    }, 1000);
+  }
+  //componentWillUnmount
+  stopTimer() {
+    clearInterval(this.timer);
+  }
+}
+
+
+class Modal {
+  constructor() {
+
+    this.inputValue = null;
+    this.render = undefined;
+    this.player = PLAYERS;
+    this.callback = null;
+  }
+
 
   subscribe(render) {
-     this.callback = render;
+    this.callback = render;
   }
 
   notify() {
-     this.callback();
+    this.callback();
   }
   inform() {
     console.log(this.other.map(e => e.text));
     this.render();
- }
- addPlayer(text) {
+  }
+  addPlayer(text) {
     this.other.push({
-       id: Utils.uuid(),
-       text: text,
-       completed: false
+      id: Utils.uuid(),
+      text: text,
+      completed: false
     });
     this.inform();
- }
- updatePlayer(index, todo) {
+  }
+  updatePlayer(index, todo) {
     this.other[index] = todo;
     this.inform();
- }
+  }
 }
-   
-  
-    
 
-const Header = (props)=>{
- let points =props.players.map((e) => e.score).reduce((a,b) =>{return a+ b});
- return(
-   <div className="header">
-   
-       <table className="stats">
-         <tr><td>PLAYERS:</td>{props.players.length}</tr>
-         <tr><td>TOTAL:</td>{points}</tr>
-         </table>
-         <h1>SCOREBOARD</h1>
-         
-         <div className="stopwatch">
-           <h2>STOPWATCH</h2>
-           <div className="stopwatch-time"></div>
-           <p><button >START</button>  <button>RESET</button></p>
-           
-           </div>
-        </div>  
- )
+
+
+
+const Header = (props) => {
+  let points = props.players.map((e) => e.score).reduce((a, b) => { return a + b });
+  return (
+    <div className="header">
+
+      <table className="stats">
+        <tr><td>PLAYERS:</td>{props.players.length}</tr>
+        <tr><td>TOTAL:</td>{points}</tr>
+      </table>
+      <h1>SCOREBOARD</h1>
+
+      <div className="stopwatch">
+        <h2>STOPWATCH</h2>
+        <div className="stopwatch-time" id="contador">
+          <Timer/>
+        </div>
+       
+
+      </div>
+    </div>
+  )
 }
-const PlayerList=(props)=>{
-  return(<div>{
-    props.players.map((data,index)=>{
-      return(
+const PlayerList = (props) => {
+  return (<div>{
+    props.players.map((data, index) => {
+      return (
         <div className="player">
           <div className="player-name">{data.name}</div>
           <div className="player-socore counter">
             <button className="counter-action decrement btn">-</button>
-              <p className="counter-score">{data.score}</p>
-              <button className="counter-action increment btn">+</button>
-              </div>
-              </div>
-      
+            <p className="counter-score">{data.score}</p>
+            <button className="counter-action increment btn">+</button>
+          </div>
+        </div>
+
       )
     })} </div>)
 }
 
-let PlayerForm= React.createClass({
-  render: function(){
-    return(
+let PlayerForm = React.createClass({
+  render: function () {
+    return (
       <div className="add-player-form">
         <form>
           <p><input type="text" placeholder="ENTER A NAME"></input></p>
           <p><input type="submit" value="Add Player"></input></p>
-    </form>
-    </div>
+        </form>
+      </div>
     )
   }
 })
 
 
-const Application = ({title, players}) => {
-   return (
-     <div className="scoreboard">
-      <Header players={players}/>
-      <PlayerList players={players}/>
-      <PlayerForm />      
-   </div>
-   ) ;
+const Application = ({ title, players }) => {
+  return (
+    <div className="scoreboard">
+      <Header players={players} />
+      <PlayerList players={players} />
+      <PlayerForm />
+    </div>
+  );
 }
-//Creando el time
 
-ReactDOM.render(<Application title="Scoreboard" players = {PLAYERS}/>, document.getElementById('container'));
+
+
+
+ReactDOM.render(<Application title="Scoreboard" players={PLAYERS} />, document.getElementById('container'));
